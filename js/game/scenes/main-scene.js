bb6405da9837d7eff4c10a1845ea3febbf458760
Lotusey <lhirias@gmail.com>
2026-07-11 10:56:28 -0400

    Created animation for main character walking

--- js/game/scenes/main-scene.js
+++ js/game/scenes/main-scene.js
@@ -11,7 +11,11 @@ export class MainScene extends Phaser.Scene {
     preload() {
         console.log('preload method called');
         this.load.image('background', 'assets/tilemaps/example_pewter2.png');
-        this.load.image('player', 'assets/sprites/MeStanding.png');
+        //this.load.image('player', 'assets/sprites/MeStanding.png');
+        this.load.spritesheet('player', 'assets/sprites/walking_animation.png', {
+            frameWidth: 100,
+            frameHeight: 90,
+        });
     }
     
     create() {
@@ -21,15 +25,95 @@ export class MainScene extends Phaser.Scene {
         const background = this.add.image(0, 0, 'background');
         background.setPosition(gameWidth / 2, gameHeight / 2);
 
-        const player = this.add.image(0, 0, 'player');
-        player.setDepth(2);
-        player.setPosition(gameWidth / 2, gameHeight / 2);
-        player.setOrigin(0.5,0.5);
-        player.setScale(0.25);
+        this.player = this.add.sprite(gameWidth / 2, gameHeight / 2, 'player');
+        this.player.setDepth(2);
+        //this.player.setPosition(gameWidth / 2, gameHeight / 2);
+        this.player.setOrigin(0.5,0.5);
+        this.player.setScale(0.25);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.keys = this.input.keyboard.addKeys({
+            W: Phaser.Input.Keyboard.KeyCodes.W,
+            A: Phaser.Input.Keyboard.KeyCodes.A,
+            S: Phaser.Input.Keyboard.KeyCodes.S,
+            D: Phaser.Input.Keyboard.KeyCodes.D,
+        });
+
+        this.anims.create({
+                key: 'walk_down',
+                frames: this.anims.generateFrameNumbers('player', {
+                    frames: [0, 1, 2],
+                }),    
+                frameRate: 6,
+                repeat: 0,     
+        });
+
+        this.anims.create({
+                key: 'walk_up',
+                frames: this.anims.generateFrameNumbers('player', {
+                    frames: [3, 4, 5],
+                }),    
+                frameRate: 6,
+                repeat: 0,     
+        });
+
+        this.anims.create({
+                key: 'walk_left',
+                frames: this.anims.generateFrameNumbers('player', {
+                    frames: [6, 7, 8],
+                }),    
+                frameRate: 6,
+                repeat: 0,     
+        });
+
+        this.anims.create({
+                key: 'walk_right',
+                frames: this.anims.generateFrameNumbers('player', {
+                    frames: [9, 10, 11],
+                }),    
+                frameRate: 6,
+                repeat: 0,     
+        });
     }
     
-    update() {
+    update(time, delta) {
         console.log('update method called');
+
+        const speed = 60; 
+        let velocityX = 0;
+        let velocityY = 0;
+
+        if (this.cursors.left.isDown || this.keys.A.isDown){
+            velocityX = -speed;
+            this.player.play('walk_left', true);
+        }
+        else if (this.cursors.right.isDown || this.keys.D.isDown){
+            velocityX = speed;
+            this.player.play('walk_right', true);
+        }
+
+        if (this.cursors.up.isDown || this.keys.W.isDown){
+            velocityY = -speed;
+            this.player.play('walk_up', true);
+        }
+        else if (this.cursors.down.isDown || this.keys.S.isDown){
+            velocityY = speed;
+            this.player.play('walk_down', true);
+        }
+        if (velocityX == 0 && velocityY == 0){
+            this.player.stop();
+        }
+
+    //normalizing diagonal speed
+    if (velocityX !== 0 || velocityY !== 0){
+        const magnitude = Math.sqrt(velocityX * velocityX + velocityY * velocityY);
+        velocityX = (velocityX / magnitude) * speed;
+        velocityY = (velocityY / magnitude) * speed;
+    }
+
+    this.player.x += velocityX * (delta / 1000);
+    this.player.y += velocityY * (delta / 1000);
+
     }
 
 }
